8a4baa14590ad048098cc1b28aa8d0bfadd5c01e
Ankit389 <ankitpankaj8044@gamil.com>
2024-09-11 20:04:13 +0530

    Initial commit

---
+++ Day1.cpp
@@ -0,0 +1,58 @@
+ #include <iostream>
+ using namespace std;
+ //   This  is    start  a  potin
+   
+ int main(){
+    // cout<< "smvdu /n  form  shir mata vaishno devi ";
+
+    //  int age  = 23; 
+     
+    //  char    garde ='1';
+     
+
+    //  char   grade =  'a';
+    //  int value ='23';
+
+//         int age;
+
+// cout <<"Ether  the value:";
+// cin>>age;
+ 
+
+//  cout <<"you    enter the  age ="<<age<<endl;
+
+
+//  variables
+
+        // int a =5,b=10;
+
+        // cout <<"sun="<<(a+b)<<endl;
+        // cout <<"sun="<<(a-b)<<endl;
+        // cout <<"sun="<<(a*b)<<endl;
+        // cout <<"sun="<<(a/b)<<endl;
+  
+
+  ///   operators
+        // cout <<(2>5)<<endl;
+        // cout <<(2==5)<<endl;
+
+
+
+        int a,b;
+
+        cout <<"enter the a :";
+        cin>>a;
+
+        cout <<"enter  the b : ";
+        cin>> b ;
+         
+        int  sum  = a+b;
+        cout<<"sum =" <<sum<<endl;
+         return 0;
+    return 0;
+
+
+ }
+
+
+ 
