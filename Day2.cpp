2249c6d00ce28cc086711703f4eb996f5f7f977c
Ankit389 <ankitpankaj8044@gamil.com>
2024-09-12 16:39:34 +0530

    loops

---
+++ Day2.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+ 
+  int main(){
+//   {
+//        int age;
+//        cout<<"enter the age";
+
+//        cin>>age;
+
+//        if(age>=18)
+//        {
+//         cout<<"you can  vote\n";
+
+//        }else{
+//         cout<<"you can'tvote\n";
+//        }
+
+  //  even   && odd   probalm 
+
+
+
+    //  int n;
+    //   cout<<" enter numbar :";
+
+    //   cin>>n;
+
+    //   if(n%2==0)
+    //   {
+    //     cout<<"enve\n";
+
+    //   }else{
+    //     cout <<"odd\n";
+    //   }
+       
+    // return 0;
+
+            //  int  marks;
+            //   cout<<"  enter the  marks";
+
+            //   cin>>marks;
+              
+            //     if(marks >=90)
+            //   {
+            //     cout<<"A\n";
+
+            //   } else if(marks >= 80 && marks <90)
+            //   {
+            //     cout<<"B\n";
+
+
+            //   }else{
+            //     cout <<"c\n";
+            //   }
+
+            //    return 0;
+
+    //  UPERCASE &&&     LOWERCASE
+
+    //   char  ch;
+    //   cout<<"enter char";
+    //   cin>>ch;
+
+    //   if(ch  >=65 &&  ch<=90){
+    //      cout <<"upercase\n";
+
+    //   }else{
+    //     cout<<"lowercase\n";
+    //   }
+
+      
+
+//   /// loops
+//    int n =  10;
+
+//     int count =1;
+//      while(count  <=n){
+//         cout<<count << " ";
+//         count++;
+//      }
+//      cout << endl;
+//       return 0;
+    
+
+
+    
+
+  }
