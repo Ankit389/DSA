9f194dfcf78ae716bb2e8ffe60866bc73a307edc
Ankit389 <ankitpankaj8044@gamil.com>
2024-10-21 18:21:29 +0530

    add new Painter's portiion probleam

---
+++ Day5.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using  namespace std;
+
+
+
+
+void changeArr(int arr[], int size){
+    cout << "in function" << endl;
+    
+}
+
+int  main(){
+    
+
+    // int  marks [5] = {1,2,3,4,5};
+    //    cout <<marks[0]<<endl;
+    //    cout <<marks[1]<<endl;
+    //    cout <<marks[2]<<endl;
+    //    cout <<marks[3]<<endl;
+
+
+
+
+    // int nums[] = {5,24,25,67,1,-15,29};
+
+    // int  size  = 7;
+
+    //  int  samllest = INT_MAX;
+
+    // for(int i=0; i<size; i++){
+    //     if(nums[i]< samllest) {
+            
+    //          samllest = nums[i];
+    //     }
+    // }
+
+    //       cout<< "samllest = " <<samllest <<endl;
+
+
+
+    int arr[]={1,2,3}
+
+ return 0 ;
+}
